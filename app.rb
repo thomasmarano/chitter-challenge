a58a1a865fadd471d0325bae460e2adb16278f14
thomasmarano <thomas.marano2@gmail.com>
2019-02-03 16:55:49 +0000

    Successfully added DB

--- app.rb
+++ app.rb
@@ -5,30 +5,16 @@ require_relative 'lib/peep'
 class PeepManager < Sinatra::Base
 
   get '/' do
-      #$peep = Peep.new
       erb (:index)
   end
-
-#enable :sessions
 
   get '/list-peeps' do
       @peeps = Peep.all
       erb (:view_add_peeps)
   end
 
-  # <% if $peep.list_of_peeps.length > 0 %>
-  #     <% $peep.list_of_peeps.each do |each_peep| %>
-  #         <ul>
-  #             <li><%=each_peep%></li>
-  #         </ul>
-  #     <% end %>
-  # <% end %>
-
   post '/save-peeps' do
-      # p 'TESTING NEW'
-      # p params[:peepshow]
       Peep.add_peep_db(params[:peepshow])
-      #$peep.add_peep(params[:peepshow])
       redirect '/list-peeps'
   end
 
--- spec/units/peep_spec.rb
+++ spec/units/peep_spec.rb
@@ -4,39 +4,22 @@ describe Peep do
     subject(:peep) {Peep.new}
     let(:single_peep) {double(:single_peep)}
 
-    # describe '#list_of_peeps' do
-    #     it 'responds to list_of_peeps' do
-    #         expect(peep).to respond_to(:list_of_peeps)
-    #     end
-    # end
-
-    # describe '#add_peep' do
-    #     it 'adds a peep to peep array' do
-    #         expect{print peep.add_peep(:single_peep)}.to output{[:single_peep]}.to_stdout
-    #     end
-    # end
-
     describe '#add_peep_db' do
         it 'adds a peep to the database' do
             connection = PG.connect(dbname: 'peep_database_test')
-            Peep.add_peep_db('TEST')
+
+            result = Peep.add_peep_db('TEST 1')
+            Peep.add_peep_db('TEST 2')
+            Peep.add_peep_db('TEST 3')
+
             peeps = Peep.all
-            expect(peeps).to include('TEST')
+
+            expect(peeps.length).to eq(3)
+            expect(peeps.first).to eq(result.first['peep'])
+            expect(peeps.first).to eq('TEST 1')
         end
     end
+
+
+
 end
-#
-# connection = PG.connect(dbname: 'complete_test_bm')
-#
-# bookmark = Bookmark.create('https://www.twitter.com', 'Twitter')
-# Bookmark.create('https://www.google.com', 'Google')
-# Bookmark.create('https://www.facebook.com', 'Facebook')
-#
-# bookmarks = Bookmark.all
-# p 'TEST'
-# p bookmark
-#
-# expect(bookmarks.length).to eq(3)
-# expect(bookmarks.first).to eq(bookmark)
-# expect(bookmarks.first.url]).to eq(bookmark['url'])
-# expect(bookmarks.first.title).to eq(bookmark['title'])
